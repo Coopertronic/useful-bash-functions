09b439f63dcd8b75bc06c4bd891c8f3f26cf46fa
Matthew Cooper <coopertronics@gmail.com>
2023-09-24 16:52:03 +0100

    Added check_root function.

--- functions.sh
+++ functions.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 ##  Some useful functions
+##  Copy this script to your project folder
+##  and add as 'source ./functions.sh' then
+##  just call the functions as normal.
+
+##> Checks if the user is root and kills the script if it is not.
+check_root() {
+    if [ "$EUID" -ne 0 ]; then
+        echo "You need to run as ROOT, dummy!!!"
+        exit 1
+    fi
+}
+
